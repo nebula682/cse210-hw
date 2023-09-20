43b4c209c398dc837084ed29c0ae1797649575b6
nebula682 <malcolmnie@gmail.com>
2023-09-20 00:11:47 -0700

    input

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,24 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+
+     {
+        // getting first name from user
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        //getting last name from user
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        // getting the results
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
+
+
+    
+        
+    
